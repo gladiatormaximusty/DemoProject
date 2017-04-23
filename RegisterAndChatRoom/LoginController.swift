b2a1e64e28fdc5f19b44d84fd1e9eb93c19ebcf0
Tian Yu <gladaitormaximusty99@gmail.com>
2017-04-23 12:38:18 +0800

    加了一個LoginController，並且只設定基本的Login button

---
+++ RegisterAndChatRoom/LoginController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  RegisterAndChatRoom
+//
+//  Created by Tian Yu on 2017/4/22.
+//  Copyright © 2017年 Edward. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    @IBAction func LoginButton(_ sender: UIButton) {
+        
+        LoginButton()
+    }
+    
+    
+    
+    func LoginButton() {
+        print("login")
+    }
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    
+    
+    
+    
+    }
+
+   
+}
+
