fa8020148a4a64edc31115715e22d16a7b3eb119
Tian Yu <gladaitormaximusty99@gmail.com>
2017-04-23 14:44:20 +0800

    登入連線Firebase完成，並建立一個新的group，設置進入登入成功後的主畫面

--- RegisterAndChatRoom/LoginController.swift
+++ RegisterAndChatRoom/LoginController.swift
@@ -7,18 +7,35 @@
 //
 
 import UIKit
-
+import Firebase
 class LoginController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBAction func LoginButton(_ sender: UIButton) {
         
-        LoginButton()
+        handleLogin()
     }
     
     
     
-    func LoginButton() {
-        print("login")
+    func handleLogin() {
+        guard let email = emailTextField.text, let password = passwordTextField.text else{
+            return
+        }
+        FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+            if error != nil {
+                print("跳出警告控制器提醒使用者ex:帳號密碼不符！！請重新輸入！！")
+                return
+            }
+            
+            
+            
+            print("login successfully")
+        })
+        
+        
+        
     }
     
     
